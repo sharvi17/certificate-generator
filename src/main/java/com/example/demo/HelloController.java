422a14c8049ee421cd13534c5aefb7078124541e
sharvi17 <162879679+sharvi17@users.noreply.github.com>
2026-04-16 20:31:43 +0530

    Change endpoint to '/api/tests' in HelloController

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @GetMapping("/")
+    @GetMapping("/api/tests")
     public String home() {
         return "Backend is running successfully 🚀";
     }
